2f2fef1872ea4c6d2b787eb4d1942045657f6929
ISCIrvingDev <ISCIrvingDev@gmail.com>
2022-03-07 23:26:14 -0600

    Adding aliases

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nodeExternals = require('webpack-node-externals');
+
+/**
+ * @param {Object} options The default webpack configuration
+ * @param {Object} webpack The reference to the underlying webpack package used by the Nest CLI
+ * @returns {Object} The webpack configuration
+ */
+module.exports = function (options /*, webpack*/) {
+  return {
+    ...options,
+    entry: ['webpack/hot/poll?100', options.entry],
+    externals: [
+      nodeExternals({
+        allowlist: ['webpack/hot/poll?100'],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@app': joinSrc('application'),
+        '@mod': joinSrc('modules'),
+      },
+    },
+  };
+};
